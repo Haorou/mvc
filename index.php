32a5354aae6f6f0a1555aea8e2fa79a21cebf258
haorou <julien.brulle@gmail.com>
2018-09-21 10:57:19 +0200

    erase old PHP and update new ones - all in english now and use <?= ?> instead of <?php echo ?>

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-    require("modele.php");
+    require("model.php");
 
-    $req = getBillets();
+    $posts = getPosts();
 
-    require('affichageAccueil.php');
+    require('indexView.php');
 ?>
--- indexView.php
+++ indexView.php
@@ -11,19 +11,19 @@
         <p>Derniers billets du blog :</p>
  
         
-        <?php while ($donnees = $req->fetch()) { ?>
+        <?php while ($data = $posts->fetch()) { ?>
             <div class="news">
                 <h3>
-                    <?php echo htmlspecialchars($donnees['titre']); ?>
-                    <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+                    <?= htmlspecialchars($data['title']) ?>
+                    <em>le <?= $data['creation_date_fr'] ?></em>
                 </h3>
                 
                 <p>
-                    <?php echo nl2br(htmlspecialchars($donnees['contenu'])); ?> <br />
+                    <?= nl2br(htmlspecialchars($data['content'])) ?>
+                    <br />
                     <em><a href="#">Commentaires</a></em>
                 </p>
             </div>
-        <?php } $req->closeCursor();?>
-
+        <?php } $posts->closeCursor(); ?>
     </body>
 </html>
